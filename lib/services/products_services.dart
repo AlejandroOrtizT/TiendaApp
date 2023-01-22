dab851c5ea51d27c0dab381fac6468c16a7d2cf9
ALEJANDRO ORTIZ <alejandro.ortiz.t@hotmail.com>
2023-01-21 20:19:39 -0600

    Segundo

--- lib/services/products_services.dart
+++ lib/services/products_services.dart
@@ -1,4 +1,7 @@
 
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:tiendaapp/models/model.dart';
 import 'package:http/http.dart' as http;
@@ -8,7 +11,7 @@ class ProductsServices extends ChangeNotifier{
   final List<Product> productos = [];
 
   late Product selectProduct;
-
+  File? newPictureFile;
   bool isLoading = true;
   bool isSaving = false;
   ProductsServices(){
@@ -40,6 +43,7 @@ class ProductsServices extends ChangeNotifier{
     notifyListeners();
     if(product.id != null){
        var url = Uri.http(_baseUrl, 'api/Productos/${product.id}');
+       product.imagen = 'View/img/productos/' + product.imagen!;
        var body = product.toJson();
        final resp = await http.put(url,body:body,headers:{"Content-Type": "application/json"});
         final decodeData = resp.body;
@@ -54,5 +58,42 @@ class ProductsServices extends ChangeNotifier{
 
 
 
+  void ActualizarImagenSeleccionada(String path){
 
+    this.selectProduct.imagen = path;
+    this.newPictureFile = File.fromUri(Uri(path: path));
+
+    notifyListeners();
+
+  }
+
+  Future<String?>  SubirImagen() async{
+    if(this.newPictureFile == null) return null;
+    this.isSaving = true;
+    notifyListeners();
+
+    final url = Uri.http(_baseUrl, 'api/subirImagen');
+
+    final imagenUploadRequest = http.MultipartRequest('POST',url);
+
+    final file = await http.MultipartFile.fromPath('file', newPictureFile!.path);
+
+    imagenUploadRequest.files.add(file);
+
+    final streamResponse = await imagenUploadRequest.send();
+
+    final resp = await http.Response.fromStream(streamResponse);
+
+    if(resp.statusCode != 200 && resp.statusCode != 201){
+      print(resp.body);
+      return null;
+    }
+
+    this.newPictureFile = null;
+
+  final decodeData = jsonDecode(resp.body);
+
+  return decodeData['url'];
+
+  }
 }
